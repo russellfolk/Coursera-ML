b3c4435cea70d616e3d1f6221729748486598376
Russell Folk <russell@clevercoder.guru>
2016-08-13 22:38:43 -0600

    Successfully implemented regularized cost function
    One more assignment this week.

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,6 +17,22 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% Compute the cost of theta. Sum through each feature, then divide by the number
+% of features: J = 1/m * sum(-y*log(h(x))-(1-y)log(1-h(x)))
+running_sum = 0;
+for index = 1:m
+	h_t = sigmoid(dot(theta', X(index,:)));
+	running_sum += (-y(index,:)*log(h_t) - (1-y(index,:))*log(1-h_t));
+end
+
+
+% for regularization, do not regularize theta_0 (in Octave, theta_1)
+regularized_sum = 0;
+for index = 2:size(theta)
+	regularized_sum += theta(index,:) .^ 2;
+end
+	
+J = (1/m) * running_sum + (lambda/(2*m)) * regularized_sum;
 
 
 
